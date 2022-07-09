617c5b13fd1d06d2a87cbd3f28ae4df048b8b63a
John Vernon <90278797+JTVernon@users.noreply.github.com>
2022-07-09 11:40:48 -0400

    Add files via upload

---
+++ utils/fetchApi.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const baseUrl ="https://bayut.p.rapidapi.com"
+
+export const fetchApi = async (url) => {
+    const { data } = await axios.get((url), {
+        headers: {
+    'X-RapidAPI-Key': //Paste API Key Here
+    'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+    }
+  });
+
+  return data;
+}
